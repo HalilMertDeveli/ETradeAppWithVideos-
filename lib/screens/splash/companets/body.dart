3fade3ce79e7a4b733748e4a44d0536fb1729954
Halil Mert Develi <halilmertdeveliiii@gmail.com>
2022-12-10 19:22:38 +0300

    we tried to do it and i was succes

--- lib/screens/splash/companets/body.dart
+++ lib/screens/splash/companets/body.dart
@@ -1,3 +1,4 @@
+import 'package:e_trade_app_with_flutter_way/constraints.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatefulWidget {
@@ -10,6 +11,42 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Column();
+    final Size size = MediaQuery.of(context).size;
+    return Align(
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.05,
+          ),
+          Text(
+            'TOKOTO',
+            style: TextStyle(color: kTextColor, fontSize: 40),
+          ),
+          SizedBox(
+            height: size.height * 0.05,
+          ),
+          Text(
+            'Welcome the TOKOTO shop ,lets shop',
+            style: TextStyle(fontSize: 17),
+          ),
+          Image.asset(
+            'assets/images/splash_1.png',
+            width: size.width * 0.7,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(49),
+            child: Container(
+              width: size.width * 0.7,
+              child: MaterialButton(
+                color: Colors.orange,
+                onPressed: () {},
+                child: Text('Continue'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
