b5fd9fd49efb2b16b15c9cb2caa1edb38af4d3ba
Halil Mert Develi <halilmertdeveliiii@gmail.com>
2022-12-10 19:05:52 +0300

    we are trying to do it before i will go back here

---
+++ android/app/src/main/kotlin/com/example/e_trade_app_with_flutter_way/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_trade_app_with_flutter_way
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:e_trade_app_with_flutter_way/constraints.dart';
+import 'package:e_trade_app_with_flutter_way/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: "Muli",
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+            bodyText1: TextStyle(color: kTextColor),
+            bodyText2: TextStyle(color: kTextColor)),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
