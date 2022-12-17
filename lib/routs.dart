9d0b982bda66de5dfbcf14c6f50cbc8f94bbd7ba
ted <alone_coder@gmial.com>
2022-12-17 18:45:41 +0300

    Revert "we got mistake video"
    This reverts commit 0185fb5ea5e5d505ef74055179c38ed10e3df94c.

--- lib/routs.dart
+++ lib/routs.dart
@@ -1,7 +1,3 @@
-import 'package:e_trade_app_with_flutter_way/screens/forgot_password/forgot_password_screen.dart';
-import 'package:e_trade_app_with_flutter_way/screens/login_success/login_success.screen.dart';
-import 'package:e_trade_app_with_flutter_way/screens/sign_in/sign_in_screen.dart';
-import 'package:e_trade_app_with_flutter_way/screens/sign_up/sign_up_screen.dart';
 import 'package:e_trade_app_with_flutter_way/screens/splash/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -10,8 +6,4 @@ import 'package:flutter/cupertino.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
-  SignInScreen.routeName: (context) => SignInScreen(),
-  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
-  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
-  SignupScreen.routeName: (context) => SignupScreen(),
 };
