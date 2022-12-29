3e23dbe161975c5e82215d73004536f845abeb64
ted <alone_coder@gmial.com>
2022-12-29 09:03:50 +0300

    we got another class for sign form and we will do Social media icons 21.14 video time

--- lib/screens/sign_in/companents/body.dart
+++ lib/screens/sign_in/companents/body.dart
@@ -2,6 +2,7 @@ import 'package:e_trade_app_with_flutter_way/companents/custom_suffix_icon.dart'
 import 'package:e_trade_app_with_flutter_way/companents/default_button.dart';
 import 'package:e_trade_app_with_flutter_way/companents/form_error.dart';
 import 'package:e_trade_app_with_flutter_way/constraints.dart';
+import 'package:e_trade_app_with_flutter_way/screens/sign_in/companents/sign_from.dart';
 import 'package:e_trade_app_with_flutter_way/size_config.dart';
 import 'package:flutter/material.dart';
 
@@ -35,131 +36,5 @@ class Body extends StatelessWidget {
         ),
       ),
     ));
-  }
-}
-
-class SignForm extends StatefulWidget {
-  const SignForm({super.key});
-
-  @override
-  State<SignForm> createState() => _SignFormState();
-}
-
-class _SignFormState extends State<SignForm> {
-  final _formKey = GlobalKey<FormState>();
-  final List<String> errors = [];
-  String? email;
-  String? password;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          buildEmailTextField(),
-          SizedBox(
-            height: getProportionateScreenHeight(20),
-          ),
-          buildPassworFormField(),
-          SizedBox(
-            height: getProportionateScreenHeight(20),
-          ),
-          FormError(errors: errors),
-          SizedBox(
-            height: getProportionateScreenHeight(20),
-          ),
-          DefaultButton(
-            text: 'Continue',
-            press: () {
-              if (_formKey.currentState!.validate()) {
-                _formKey.currentState!.save();
-              }
-            },
-          )
-        ],
-      ),
-    );
-  }
-
-  TextFormField buildPassworFormField() {
-    return TextFormField(
-      onSaved: (newValue) => password = newValue,
-      onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(kPassNullError)) {
-          setState(() {
-            errors.remove(kPassNullError);
-          });
-        } else if (value.length >= 8 && errors.contains(kShortPassError)) {
-          setState(() {
-            errors.remove(kShortPassError);
-          });
-        }
-        return null;
-      },
-      validator: (value) {
-        if (value != null) {
-          if (value.isEmpty && !errors.contains(kPassNullError)) {
-            setState(() {
-              errors.add(kPassNullError);
-            });
-          } else if (value.length < 8 && !errors.contains(kShortPassError)) {
-            setState(() {
-              errors.add(kShortPassError);
-            });
-          }
-        }
-        return null;
-      },
-      obscureText: true,
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-          labelText: 'Password',
-          hintText: 'Enter your Password',
-          suffixIcon: CostumSuffixIcon(
-            svgIcon: 'assets/icons/Lock.svg',
-          )),
-    );
-  }
-
-  TextFormField buildEmailTextField() {
-    return TextFormField(
-      onSaved: (newValue) => email = newValue,
-      onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.remove(kEmailNullError);
-          });
-        } else if (emailValidatorRegExp.hasMatch(value) &&
-            errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.remove(kInvalidEmailError);
-          });
-        }
-        return null;
-      },
-      validator: (value) {
-        if (value != null) {
-          if (value.isEmpty && !errors.contains(kEmailNullError)) {
-            setState(() {
-              errors.add(kEmailNullError);
-            });
-          } else if (!emailValidatorRegExp.hasMatch(value) &&
-              !errors.contains(kInvalidEmailError)) {
-            setState(() {
-              errors.add(kInvalidEmailError);
-            });
-          }
-        }
-        return null;
-      },
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        labelText: 'E-mail',
-        hintText: "Enter your e-mail ",
-        // floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CostumSuffixIcon(svgIcon: 'assets/icons/Mail.svg'),
-      ),
-    );
   }
 }
---
+++ lib/screens/sign_in/companents/sign_from.dart
@@ -0,0 +1,149 @@
+import 'package:e_trade_app_with_flutter_way/companents/custom_suffix_icon.dart';
+import 'package:e_trade_app_with_flutter_way/companents/default_button.dart';
+import 'package:e_trade_app_with_flutter_way/companents/form_error.dart';
+import 'package:e_trade_app_with_flutter_way/constraints.dart';
+import 'package:e_trade_app_with_flutter_way/size_config.dart';
+import 'package:flutter/material.dart';
+
+class SignForm extends StatefulWidget {
+  const SignForm({super.key});
+
+  @override
+  State<SignForm> createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
+  String? email;
+  String? password;
+  bool remember = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildEmailTextField(),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          buildPassworFormField(),
+          Row(
+            children: [
+              Checkbox(
+                value: remember,
+                activeColor: kPrimaryColor,
+                onChanged: (value) {
+                  setState(() {
+                    remember = value!;
+                  });
+                },
+              ),
+              Text('Remember me'),
+              Spacer(),
+              Text(
+                'Forgot your Password',
+                style: TextStyle(decoration: TextDecoration.underline),
+              ),
+            ],
+          ),
+          FormError(errors: errors),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          DefaultButton(
+            text: 'Continue',
+            press: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+              }
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildPassworFormField() {
+    return TextFormField(
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kPassNullError)) {
+          setState(() {
+            errors.remove(kPassNullError);
+          });
+        } else if (value.length >= 8 && errors.contains(kShortPassError)) {
+          setState(() {
+            errors.remove(kShortPassError);
+          });
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value != null) {
+          if (value.isEmpty && !errors.contains(kPassNullError)) {
+            setState(() {
+              errors.add(kPassNullError);
+            });
+          } else if (value.length < 8 && !errors.contains(kShortPassError)) {
+            setState(() {
+              errors.add(kShortPassError);
+            });
+          }
+        }
+        return null;
+      },
+      obscureText: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          labelText: 'Password',
+          hintText: 'Enter your Password',
+          suffixIcon: CostumSuffixIcon(
+            svgIcon: 'assets/icons/Lock.svg',
+          )),
+    );
+  }
+
+  TextFormField buildEmailTextField() {
+    return TextFormField(
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.remove(kEmailNullError);
+          });
+        } else if (emailValidatorRegExp.hasMatch(value) &&
+            errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.remove(kInvalidEmailError);
+          });
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value != null) {
+          if (value.isEmpty && !errors.contains(kEmailNullError)) {
+            setState(() {
+              errors.add(kEmailNullError);
+            });
+          } else if (!emailValidatorRegExp.hasMatch(value) &&
+              !errors.contains(kInvalidEmailError)) {
+            setState(() {
+              errors.add(kInvalidEmailError);
+            });
+          }
+        }
+        return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'E-mail',
+        hintText: "Enter your e-mail ",
+        // floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CostumSuffixIcon(svgIcon: 'assets/icons/Mail.svg'),
+      ),
+    );
+  }
+}
