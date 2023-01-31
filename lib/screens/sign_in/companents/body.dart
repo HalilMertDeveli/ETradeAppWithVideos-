36e27919a4debbf36e9894c3cba25321f7269813
Halil Mert <halilmertdeveliii@gmail.com>
2023-01-31 13:21:52 +0300

    we finished the login in page

--- lib/screens/sign_in/companents/body.dart
+++ lib/screens/sign_in/companents/body.dart
@@ -15,61 +15,76 @@ class Body extends StatelessWidget {
       child: Padding(
         padding:
             EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-        child: Column(
-          children: [
-            Text(
-              'Welcome Back',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: getProportionateScreenHeight(28),
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.04,
               ),
-            ),
-            Text(
-              'Sign in with your e-mail and password \nor continue with social media',
-              textAlign: TextAlign.center,
-            ),
-            SignForm(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SocialCard(
-                  icon: 'assets/icons/facebook-2.svg',
-                  press: () {
-                    //operation is login with facebook
-                  },
+              Text(
+                'Welcome Back',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenHeight(28),
+                  fontWeight: FontWeight.bold,
                 ),
-                SocialCard(
-                  icon: 'assets/icons/google-icon.svg',
-                  press: () {
-                    //operation is login with google
-                  },
-                ),
-                SocialCard(
-                  icon: 'assets/icons/twitter.svg',
-                  press: () {
-                    //operation is login with twitter book
-                  },
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "Don't Have an account ? ",
-                  style: TextStyle(
-                    fontSize: getProportionateScreenWidth(16),
+              ),
+              Text(
+                'Sign in with your e-mail and password \nor continue with social media',
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.08,
+              ),
+              SignForm(),
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.08,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialCard(
+                    icon: 'assets/icons/facebook-2.svg',
+                    press: () {
+                      //operation is login with facebook
+                    },
                   ),
-                ),
-                Text(
-                  'sign up',
-                  style: TextStyle(
+                  SocialCard(
+                    icon: 'assets/icons/google-icon.svg',
+                    press: () {
+                      //operation is login with google
+                    },
+                  ),
+                  SocialCard(
+                    icon: 'assets/icons/twitter.svg',
+                    press: () {
+                      //operation is login with twitter book
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: getProportionateScreenHeight(20),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't Have an account ? ",
+                    style: TextStyle(
                       fontSize: getProportionateScreenWidth(16),
-                      color: kPrimaryColor),
-                )
-              ],
-            )
-          ],
+                    ),
+                  ),
+                  Text(
+                    'sign up',
+                    style: TextStyle(
+                        fontSize: getProportionateScreenWidth(16),
+                        color: kPrimaryColor),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ));
